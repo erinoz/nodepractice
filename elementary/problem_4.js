9d079961402a7c8349f43777439853f1c0e861de
erinozolins <erin.a.paul@gmail.com>
2020-06-19 07:30:31 -0400

    adding notes to problems 4 & 5

---
+++ elementary/problem_4.js
@@ -0,0 +1,3 @@
+/* Write a program that asks the user for a number n and prints the 
+ * sum of the numbers 1 to n
+ */
---
+++ elementary/problem_5.js
@@ -0,0 +1,3 @@
+/* Modify the previous program such that only multiples of three or 
+ * five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+ */

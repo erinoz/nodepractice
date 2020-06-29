74df869214be22f0d0c5b18525df81c91b9f830c
erinozolins <erin.a.paul@gmail.com>
2020-06-29 16:32:26 -0400

    Added solution for problem 4

--- elementary/problem_4.js
+++ elementary/problem_4.js
@@ -1,3 +1,12 @@
 /* Write a program that asks the user for a number n and prints the 
  * sum of the numbers 1 to n
  */
+
+const userInput_start = 2
+const userInput_end = 4
+let count = 0;
+for (let i = userInput_start; i <= userInput_end; i++) {
+    //console.log(`the incrementer = ${i}`)
+    count += i;
+}
+console.log(`count = ${count}`)

83db2e0d0451b0e275ed6973c07b7c976e00d77b
erinozolins <erin.a.paul@gmail.com>
2020-07-03 15:59:42 -0400

    Update to problem 5

--- elementary/problem_5.js
+++ elementary/problem_5.js
@@ -2,7 +2,7 @@
  * five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
  */
 
-const userInput = 21
+const userInput = 10
 let count = 0;
 for (let i = 0; i <= userInput; i++) {
     console.log(`looking at number = ${i}`)

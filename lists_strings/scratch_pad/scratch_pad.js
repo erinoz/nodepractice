9f37a016feaaad78b9d3b62cd3b23710f0d205e3
erinozolins <erin.a.paul@gmail.com>
2020-08-19 18:08:45 -0400

    added scratch pad file for notetaking

---
+++ lists_strings/scratch_pad/scratch_pad.js
@@ -0,0 +1,27 @@
+inputData = 
+
+
+for (let i = 0; i < *insert number of array objects here*; i++)
+  if (email = ssantiago@netflix.com) {
+    return response
+  } else {
+  }
+
+
+
+
+  for (let i = 0; i < *insert number of array objects here*; i++)
+  if (email = ssantiago@netflix.com) {
+    return response
+  } else {
+  }
+
+
+const attendeesArray = inputData.attendeesArray.split(",");
+const filteredAttendee = attendeesArray.filter(attendee => attendee.includes('ssantiago@netflix.com'));
+
+const attendeesArray = inputData.attendeesEmailArray.length;
+
+
+
+// 

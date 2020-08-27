d1906c96620517a482c81cebe4c1cc7d4a067ae3
erinozolins <erin.a.paul@gmail.com>
2020-08-27 15:57:40 -0400

    updates to scratch pad

--- lists_strings/scratch_pad/scratch_pad.js
+++ lists_strings/scratch_pad/scratch_pad.js
@@ -22,6 +22,35 @@ const filteredAttendee = attendeesArray.filter(attendee => attendee.includes('ss
 
 const attendeesArray = inputData.attendeesEmailArray.length;
 
+for (let i = 0; i < numberOfEmails; i++)
+  if (attendeesEmailArray[i] === ssantiago@netflix.com && attendeesResponses === "accepted") {
+    const santiagoAttending = true;
+  } else {
+  }; 
 
+output = [{attendeesEmailArray: attendeesEmailArray, number_of_emails: numberOfEmails, santiago_attending: santiagoAttending}];
 
 // 
+
+var attendeesEmailArray = inputData.attendeesEmails.split(','); 
+var attendeesResponsesArray = inputData.attendeesResponses.split(','); 
+var numberOfEmails = attendeesEmailArray.length; 
+var santiagoAttending = 'didnt check yet'; 
+
+var santiagoPosition = attendeesEmailArray.indexOf("ssantiago@netflix.com");
+console.log(santiagoPosition)
+
+var santiagoResponse = attendeesResponsesArray[santiagoPosition]
+console.log(santiagoResponse)
+
+//for (var i = 0; i < numberOfEmails; i++) {
+  //if (numberOfEmails < 10) {
+    //var santiagoAttending = 'correct';
+ // } else {
+    //var santiagoAttending = 'fail';
+ // }
+//}
+
+// output = [{attendeesEmailArray: attendeesEmailArray, number_of_emails: numberOfEmails, santiago_attending: santiagoAttending}];*/
+
+output = [{attendeesEmailArray: attendeesEmailArray, number_of_emails: numberOfEmails, attendee_responses: attendeesResponsesArray, santiago_response: santiagoResponse}];

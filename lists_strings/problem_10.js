552a27602509a05576fae1f1f74d60dba0f75f35
erinozolins <erin.a.paul@gmail.com>
2020-10-16 18:42:16 -0400

    Updates to problems 10 and 11

--- lists_strings/problem_10.js
+++ lists_strings/problem_10.js
@@ -15,40 +15,67 @@ alternatingly taking elements, e.g. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 // 2 items and 3 items
 
 // Using Math.min to find smallest list length
- 
+
 const combineLists = (listOne, listTwo) => {
-    let smallestListLength = Math.min(listOne.length, listTwo.length)
-    console.log(smallestListLength)
+  let smallestListLength = Math.min(listOne.length, listTwo.length);
 
-    let combinedList = [];
+  let combinedList = [];
+  let arrayPointer;
 
-    for (i = 0; i < smallestListLength; i++) {
-        combinedList.push(listOne[i]);
-        console.log(combinedList)
-        combinedList.push(listTwo[i]);
-        console.log(combinedList)
+  for (arrayPointer = 0; arrayPointer < smallestListLength; arrayPointer++) {
+    combinedList.push(listOne[arrayPointer]);
+    combinedList.push(listTwo[arrayPointer]);
+  }
+
+  if (listOne.length > listTwo.length) {
+    for (arrayPointer; arrayPointer < listOne.length; arrayPointer++) {
+      combinedList.push(listOne[arrayPointer]);
     }
+  }
 
-    console.log("after the for loop:", combinedList)
-
-    if (listOne.length > listTwo.length) {
-        remainderOne = (listOne.length - listTwo.length) + 2;
-        console.log("remainderOne", remainderOne);
-        for (i = remainderOne; i < listOne.length; i++) {
-            combinedList.push(listOne[i]);
-        }
+  if (listOne.length < listTwo.length) {
+    for (arrayPointer; arrayPointer < listTwo.length; arrayPointer++) {
+      combinedList.push(listTwo[arrayPointer]);
     }
+  }
+  return combinedList;
+};
 
-    if (listOne.length < listTwo.length) {
-        remainderTwo = (listTwo.length - listOne.length) + 2;
-        for (i = remainderTwo; i < listTwo.length; i++) {
-            combinedList.push(listTwo[i]);
-        }
+console.log(combineLists(["a", "b", "c", "d"], [1, 2, 3, 4, 5]));
+
+const combineListsUsingRemainder = (listOne, listTwo) => {
+  let smallestListLength = Math.min(listOne.length, listTwo.length);
+
+  let combinedList = [];
+
+  for (i = 0; i < smallestListLength; i++) {
+    combinedList.push(listOne[i]);
+    combinedList.push(listTwo[i]);
+  }
+
+  if (listOne.length > listTwo.length) {
+    remainderOne = listOne.length - listTwo.length;
+    for (i = listOne.length - remainderOne; i < listOne.length; i++) {
+      combinedList.push(listOne[i]);
     }
-    return combinedList
-}
+  }
 
-console.log(combineLists(["a", "b", "c", "d"], [1, 2, 3, 4, 5]))
+  if (listOne.length < listTwo.length) {
+    remainderTwo = listTwo.length - listOne.length;
+    for (i = listTwo.length - remainderTwo; i < listTwo.length; i++) {
+      combinedList.push(listTwo[i]);
+    }
+  }
+  return combinedList;
+};
+
+console.log(combineListsUsingRemainder(["a", "b", "c", "d"], [1, 2, 3, 4, 5]));
+
+// TODO - use Math.diff to find the difference and condense to one if statement - you'll still have to account for
+// which list is being selected and the long tail is getting added to the new list 
+
+
+
 //     return newList
 // }
 
@@ -61,10 +88,10 @@ console.log(combineLists(["a", "b", "c", "d"], [1, 2, 3, 4, 5]))
 //         combinedList += listOne.shift()
 //         combinedList += listTwo.shift()
 
-    // }
+// }
 
-    // evaluate if the full lists have been returned, if not, add the remainder of the longer list
-    // to the end of the new array
+// evaluate if the full lists have been returned, if not, add the remainder of the longer list
+// to the end of the new array
 
 //     return combinedList
 // }
@@ -75,7 +102,7 @@ console.log(combineLists(["a", "b", "c", "d"], [1, 2, 3, 4, 5]))
 
 // const checkStringIsPalindrome = (str) => {
 //     let pointerOne = 0;
-//     let pointerTwo = str.length -1; 
+//     let pointerTwo = str.length -1;
 
 //     while (pointerOne <= pointerTwo) {
 //         if (str.charAt(pointerOne) !== str.charAt(pointerTwo)) {
--- lists_strings/problem_11.js
+++ lists_strings/problem_11.js
@@ -6,4 +6,42 @@ concatenating them followed by a sort.
 */
 
 // Test cases
+// 0 items each
+// 1 item each
+// 2 items each
+// 3+ items each
+// 0 items and 1 item
+// 0 items and 2 items
+// 0 items and 3 items
+// 1 item and 2 items
+// 1 item and 3 items
+// 2 items and 3 items
 
+// Loop over while pointer 1 is less than the length of the first array and pointer 2 is less than the length of the
+// second Array
+
+// Evaluate each item to select the smaller one to push into the list 
+
+// Once one of those pointers reaches the end of the array, then add the rest of the elements for 
+// the other if there's anything left
+
+const mergeSortedLists = (listOne, listTwo) => {
+    let newList = []; 
+    let pointerOne = 0;
+    let pointerTwo = 0; 
+    let i = 0;
+
+    while (pointerOne <= listOne.length && pointerTwo <= listTwo.length) {
+            if (pointerOne[i] < pointerTwo[i]) {
+                newList.push(pointerOne[i]);
+                console.log(newList);
+                i++;
+            } else {
+                newList.push(pointerTwo[i]);
+                console.log(newList);
+                i++;
+            }
+
+        
+    }
+}
